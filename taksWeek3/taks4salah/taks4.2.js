c69e64f7336661ed34c7bb14dc4bca801eb3f790
Herena <sintaherena@gmail.com>
2020-09-15 23:05:59 +0700

    taksweek3 update no4

--- taksWeek3/taks4salah/taks4.2.js
+++ taksWeek3/taks4salah/taks4.2.js
@@ -4,13 +4,13 @@ const url = 'https://jsonplaceholder.typicode.com/users'
 
 fetch(url)
  //////////////////Get All Data/////////////////////////
- .then(response=>response.json())
- .then((data) => console.log(data));
+//  .then(response=>response.json())
+//  .then((data) => console.log(data));
 
   //////////////////Get Name/////////////////////////
-  // .then(response=>response.json())
-  // .then((data) => {
-  //   return data.map((item) =>{
-  //     console.log(item.name)
-  //   })
-  // });
+  .then(response=>response.json())
+  .then((data) => {
+    return data.map((item) =>{
+      console.log(item.name)
+    })
+  });
